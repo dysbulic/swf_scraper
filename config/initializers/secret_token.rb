b841a53c4115dc8eee1d7605ae08c23c252c4b22
Will Holcomb <will@dhappy.org>
2013-07-31 16:32:41 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SwfScraper::Application.config.secret_key_base = '581dfc1124e50cc583f0069cb116c068977935ee484f3d1536f8eaf66b16925853e4c5ee80382da611114eb7b87fc13891de5afc6b3914673f303654fa945eea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SwfScraper::Application.config.session_store :cookie_store, key: '_swf_scraper_session'
